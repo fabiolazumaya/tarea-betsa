6b2642d3a9cec28ee6ae0461f58d15737e4fcc2d
Fabiola Sanchez <fabi94@live.com.mx>
2020-04-07 20:45:03 -0500

    ejercicio-dom

---
+++ profe-fer/js/ejercicio-dom.js
@@ -0,0 +1,26 @@
+//número de enlaces de la página
+
+
+
+var numenlaces= document.getElementsByTagName("a")
+console.log(numenlaces.length)
+
+var array = numenlaces[0];
+console.log(array)
+// for(var i=0; i<numenlaces.length; i++) {
+//     var x = numenlaces[i];
+// }
+// console.log(numenlaces)
+
+
+
+//dirección a la que enlaza el penúltimo enlace
+
+
+
+//número de enlaces que enlazan a http://prueba
+
+
+
+
+//número de enlaces del tercer párrafo

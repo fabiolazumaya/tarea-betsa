4048be78d6534b713b4816c05cd115e79326cc91
Fabiola Sanchez <fabi94@live.com.mx>
2020-04-23 09:16:23 -0500

    cambiando de estilos y copiando carpetas a angular para poder trabajar

--- profe-fer/js/else-if.js
+++ profe-fer/js/else-if.js
@@ -6,14 +6,4 @@ let rojo = "Detente"
 
 
 
-if(ejecucion){
-    console.log(numero1)
-}else if(ejecucion){
-    console.log(numero2){
-}else if (ejecucion){
-    console.log(numero3)
-}else if (ejecucion){
-    console.log("El semáforo se encuentra en reparación")
-}
-
-console.log(`La respuesta de la acción ${ejecucion} es ${console.log}`)
+if 
